6fbd855dfae4d0d786ddf79afebacf3580395d5a
Martin Ouimet <marouim@gmail.com>
2020-07-14 13:57:26 -0400

    Initial commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace simpleproject_api_inventory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            Product p1 = new Product();
+            p1.ID = "0001";
+            p1.Name = "Table";
+            p1.Description = "En bois, haute";
+
+            return (new List<Product> {p1}).ToArray();
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace simpleproject_api_inventory
+{
+    public class Product
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
